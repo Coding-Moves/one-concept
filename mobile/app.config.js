67dc69731868f4ba2f3a4f92c24522c0031ece7a
Muawiya Amir <contactmuawia@gmail.com>
2026-09-05 00:26:42 +0500

    Bump app version to 1.3.0 for release

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -18,7 +18,7 @@ module.exports = {
     name: 'One Concept',
     slug: 'one-concept',
     owner: 'coding-moves',
-    version: '1.2.0',
+    version: '1.3.0',
     orientation: 'portrait',
     icon: './assets/icon.png',
     userInterfaceStyle: 'automatic',
